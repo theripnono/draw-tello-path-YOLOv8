ddea8bede1cd4ee09a85dcf0ce664b62a9bfac79
David Rosset <drosset@tecbrain.com>
2023-08-13 19:29:15 +0200

    First look of exporting json of drawn path Bottons are added

--- grid.py
+++ grid.py
@@ -12,29 +12,33 @@ class Grid(Turtle):
 
     def draw_grid(self):
 
-        self.x=-370
+        self.x=-375
         self.y=300
         self.penup()
         self.pencolor("lightgreen")
         self.speed("fast")
+        self.hideturtle()
         # self.hideturtle()
         # #Horizontal
-        for grid in range(0,15):
+        for grid in range(0,21):
             self.goto(self.x,self.y)
             self.pendown()
             self.forward(600)
-            self.y -= 50
+            self.y -= 30
             self.penup()
         """reset x and y values"""
-        self.x = -370
+        self.x = -375
         self.y = 300
         #Vertical
         self.goto(self.x, self.y)
         self.right(90)
-        for grid in range(0,13):
+        for grid in range(0,21):
             self.goto(self.x,self.y)
             self.pendown()
             self.forward(600)
-            self.x += 50
+            self.x += 30
             self.penup()
+        self.hideturtle()
 
+    def clear_grid(self):
+        self.clear()
